a4d88a3eee41344621b3b440c0f03ce95b36b965
Hubert Owerczuk <hub.ower@gmail.com>
2022-09-06 08:19:33 +0200

    Add nullable slug to recipe

--- database/migrations/2022_08_24_091210_add_slug_to_recipes_table.php
+++ database/migrations/2022_08_24_091210_add_slug_to_recipes_table.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Models\Recipe;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 return new class extends Migration
 {
@@ -15,8 +17,18 @@ return new class extends Migration
     {
         Schema::table('recipes', function (Blueprint $table) {
             $table->after('name', function($table) {
-                $table->string('slug')->unique();
+                $table->string('slug')->nullable();
             });
+        });
+
+        foreach (Recipe::all() as $recipe)
+        {
+            $recipe->slug = Str::of($recipe->name)->slug('-');
+            $recipe->save();
+        }
+
+        Schema::table('recipes', function (Blueprint $table) {
+            $table->string('slug')->nullable(false)->change();
         });
     }
 
