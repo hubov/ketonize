5957a72f09be3ba07902a2cbef0b11bfc459eb6d
Hubert Owerczuk <hub.ower@gmail.com>
2022-09-20 13:37:40 +0200

    Adjust recipes to meals

--- app/Jobs/GenerateDietPlan.php
+++ app/Jobs/GenerateDietPlan.php
@@ -49,35 +49,52 @@ class GenerateDietPlan implements ShouldQueue
     protected function task(User $user)
     {
         $mealsCount = 4;
-        $meals[1] = $user->userDiet->kcal * 0.3;
-        $meals[2] = $user->userDiet->kcal * 0.3;
-        $meals[3] = $user->userDiet->kcal * 0.1;
-        $meals[4] = $user->userDiet->kcal * 0.3;
+        $meals[1] = [
+            'tag' => 1,
+            'kcal' => $user->userDiet->kcal * 0.3
+        ];
+        $meals[2] = [
+            'tag' => 2,
+            'kcal' => $user->userDiet->kcal * 0.3
+        ];
+        $meals[3] = [
+            'tag' => 4,
+            'kcal' => $user->userDiet->kcal * 0.1
+        ];
+        $meals[4] = [
+            'tag' => 3,
+            'kcal' => $user->userDiet->kcal * 0.3
+        ];
 
         $macro = $user->userDiet->protein + $user->userDiet->fat + $user->userDiet->carbohydrate;
         $protein = $user->userDiet->protein / $macro * 100;
         $fat = $user->userDiet->fat / $macro * 100;
         $carbohydrate = $user->userDiet->carbohydrate / $macro * 100;
 
+        $chosenRecipes = [];
+
         foreach ($meals as $key => $meal)
         {
-            $recipe = Recipe::whereBetween('protein_ratio', [$protein*0.5, $protein*1.5])
-                    ->whereBetween('fat_ratio', [$fat*0.5, $fat*1.5])
-                    ->whereBetween('carbohydrate_ratio', [$carbohydrate*0.5, $carbohydrate*1.5])
+            $recipe = Recipe::join('recipe_tag', 'recipes.id', '=', 'recipe_id')
+                    ->select('recipes.*')
+                    ->where('tag_id', $meal['tag'])
+                    ->whereBetween('protein_ratio', [$protein*0.5, $protein*1.5])
+                    // ->whereBetween('fat_ratio', [$fat*0.5, $fat*1.5])
+                    ->whereBetween('carbohydrate_ratio', [0, $carbohydrate*1.5])
+                    ->whereNotIn('recipes.id', $chosenRecipes)
                     ->inRandomOrder()
-                    ->get();
+                    ->first();
 
-            dd($recipe);
-
-            $modifier = round($meal / $recipe->kcal * 100);
+            $modifier = round($meal['kcal'] / $recipe->kcal * 100);
 
             $dietPlan = new DietPlan;
             $dietPlan->user_id = $user->id;
             $dietPlan->modifier = $modifier;
             $dietPlan->recipe_id = $recipe->id;
             $dietPlan->meal = $key;
-            $dietPlan->date_on = $date;
+            $dietPlan->date_on = $this->date;
             $dietPlan->save();
+            $chosenRecipes[] = $recipe->id;
         }
     }
 }
