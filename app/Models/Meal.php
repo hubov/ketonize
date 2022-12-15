8324348a6ea9fcd0397d7bcfe09f6cec3f0fbe0d
Hubert Owerczuk <hub.ower@gmail.com>
2022-12-15 11:06:21 +0100

    Add Meal model

---
+++ app/Models/Meal.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Meal extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['diet_plan_id', 'recipe_id', 'meal', 'modifier'];
+
+    public function dietPlan()
+    {
+        return $this->belongsTo(DietPlan::class);
+    }
+
+    public function recipe()
+    {
+        return $this->belongsTo(Recipe::class);
+    }
+
+    public function scale()
+    {
+        $this->recipe->protein = round($this->recipe->protein * $this->modifier / 100);
+        $this->recipe->fat = round($this->recipe->fat * $this->modifier / 100);
+        $this->recipe->carbohydrate = round($this->recipe->carbohydrate * $this->modifier / 100);
+        $this->recipe->kcal = round($this->recipe->kcal * $this->modifier / 100);
+    }
+
+    public function shares()
+    {
+        $macros = $this->recipe->protein + $this->recipe->fat + $this->recipe->carbohydrate;
+        $this->shareProtein = round($this->recipe->protein / $macros * 100);
+        $this->shareFat = round($this->recipe->fat / $macros * 100);
+        $this->shareCarbohydrate = round($this->recipe->carbohydrate / $macros * 100);
+    }
+}
---
+++ database/migrations/2022_12_15_072024_create_meals_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\DietPlan;
+use App\Models\Recipe;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meals', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(DietPlan::class);
+            $table->foreignIdFor(Recipe::class);
+            $table->tinyInteger('meal');
+            $table->smallInteger('modifier');
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meals');
+    }
+};
