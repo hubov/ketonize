0850e01a4ee3ccb457500a14d55850c0de18bf20
Hubert Owerczuk <hub.ower@gmail.com>
2023-01-04 12:54:33 +0100

    Add CRUD methods to Recipe repository

--- app/Repositories/Interfaces/RecipeRepositoryInterface.php
+++ app/Repositories/Interfaces/RecipeRepositoryInterface.php
@@ -8,4 +8,8 @@ interface RecipeRepositoryInterface
 {
     public function get(int $id) : Recipe;
     public function getBySlug(string $slug) : Recipe;
+    public function create(array $attributes) : Recipe;
+    public function update(int $id, array $attributes) : Recipe;
+    public function updateBySlug(string $slug, array $attributes) : Recipe;
+    public function delete(int $id) : bool;
 }
--- app/Repositories/RecipeRepository.php
+++ app/Repositories/RecipeRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 
 use App\Models\Recipe;
 use App\Repositories\Interfaces\RecipeRepositoryInterface;
+use Illuminate\Support\Collection;
 
 class RecipeRepository implements RecipeRepositoryInterface
 {
@@ -12,8 +13,40 @@ class RecipeRepository implements RecipeRepositoryInterface
         return Recipe::find($id)->first();
     }
 
+    public function getAll() : Collection
+    {
+        return Recipe::all();
+    }
+
     public function getBySlug(string $slug): Recipe
     {
         return Recipe::where('slug', $slug)->firstOrFail();
+    }
+
+
+    public function create(array $attributes): Recipe
+    {
+        return Recipe::create($attributes);
+    }
+
+    public function update(int $id, array $attributes): Recipe
+    {
+        $recipe = Recipe::find($id);
+        $recipe->update($attributes);
+
+        return $recipe;
+    }
+
+    public function updateBySlug(string $slug, array $attributes): Recipe
+    {
+        $recipe = Recipe::where('slug', $slug)->first();
+        $recipe->update($attributes);
+
+        return $recipe;
+    }
+
+    public function delete(int $id): bool
+    {
+        return Recipe::destroy($id);
     }
 }
