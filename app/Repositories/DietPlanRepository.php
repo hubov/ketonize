b95aa835336e8885c356761d7c0dee0b5d22a653
Hubert Owerczuk <hub.ower@gmail.com>
2023-01-11 12:59:07 +0100

    Add method to create dietPlans in bulk

--- app/Repositories/DietPlanRepository.php
+++ app/Repositories/DietPlanRepository.php
@@ -4,9 +4,18 @@ namespace App\Repositories;
 
 use App\Models\DietPlan;
 use App\Repositories\Interfaces\DietPlanRepositoryInterface;
+use App\Repositories\Interfaces\UserRepositoryInterface;
+use Illuminate\Support\Collection;
 
 class DietPlanRepository implements DietPlanRepositoryInterface
 {
+    protected $userRepository;
+
+    public function __construct(UserRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
     public function get(int $id) : DietPlan
     {
         return DietPlan::find($id);
@@ -18,4 +27,30 @@ class DietPlanRepository implements DietPlanRepositoryInterface
                         ->where('date_on', $date)
                         ->firstOrNew();
     }
+
+    public function createForUserBulk(int $userId, array $datesList): Collection
+    {
+        $user = $this->userRepository->get($userId);
+
+        $user->dietPlans()
+            ->createMany(
+                $this->arrangeBulk($userId, $datesList)
+            );
+
+        return $user->dietPlans;
+    }
+
+    protected function arrangeBulk(int $userId, $datesList)
+    {
+        $dietPlans = [];
+        foreach ($datesList as $date) {
+            $dietPlans[] = [
+                'user_id' => $userId,
+                'date_on' => $date
+            ];
+        }
+
+        return $dietPlans;
+    }
+
 }
--- app/Repositories/Interfaces/DietPlanRepositoryInterface.php
+++ app/Repositories/Interfaces/DietPlanRepositoryInterface.php
@@ -3,10 +3,12 @@
 namespace App\Repositories\Interfaces;
 
 use App\Models\DietPlan;
-use App\Models\User;
+use Illuminate\Support\Collection;
 
 interface DietPlanRepositoryInterface
 {
+    public function __construct(UserRepositoryInterface $userRepository);
     public function get(int $id) : DietPlan;
     public function getByDate(int $userId, string $date) : DietPlan;
+    public function createForUserBulk(int $userId, array $datesList) : Collection;
 }
