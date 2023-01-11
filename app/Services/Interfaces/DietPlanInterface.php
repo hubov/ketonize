b358285e4cc4d76d00458808226a056b4379c912
Hubert Owerczuk <hub.ower@gmail.com>
2023-01-11 13:02:21 +0100

    Add missing setter for user in interface for DietPlan service

--- app/Services/Interfaces/DietPlanInterface.php
+++ app/Services/Interfaces/DietPlanInterface.php
@@ -3,6 +3,7 @@
 namespace App\Services\Interfaces;
 
 use App\Models\DietPlan;
+use App\Models\User;
 use App\Repositories\Interfaces\DietPlanRepositoryInterface;
 use App\Repositories\Interfaces\MealRepositoryInterface;
 use App\Repositories\Interfaces\RecipeRepositoryInterface;
@@ -10,6 +11,7 @@ use App\Repositories\Interfaces\RecipeRepositoryInterface;
 interface DietPlanInterface
 {
     public function __construct(DietPlanRepositoryInterface $dietPlanRepository, RecipeRepositoryInterface $recipeRepository, MealRepositoryInterface $mealRepository);
+    public function setUser(User $user);
     public function getByDate($date) : DietPlan;
     public function getDates() : array;
 }
