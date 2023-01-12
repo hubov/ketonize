67d26476fdf329850fbaf8d9bfdbe199c9fca44e
Hubert Owerczuk <hub.ower@gmail.com>
2023-01-12 15:13:04 +0100

    Fix scheduled job for generating DietPlans daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,9 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call(function() {
-            (new GenerateDietPlan)->handle();
-        })->daily();
+        $schedule->job(new GenerateDietPlan())->daily();
     }
 
     /**
