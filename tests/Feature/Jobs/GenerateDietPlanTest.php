c4e83f28256ef1c73a08dc3290b569c0b29a65a5
Hubert Owerczuk <hub.ower@gmail.com>
2023-01-12 15:15:57 +0100

    Comment out redundant tests for GenerateDietPlan job

--- tests/Feature/Jobs/GenerateDietPlanTest.php
+++ tests/Feature/Jobs/GenerateDietPlanTest.php
@@ -18,73 +18,72 @@ class GenerateDietPlanTest extends TestCase
      *
      * @return void
      */
-    public function test_handle_with_a_date_and_a_user_given()
-    {
-        $user = User::factory()->create();
-        $tags = $user->userDiet->getMealsTags();
-        $recipes = Recipe::factory()->has(Tag::factory())->count(4)->create();
-        foreach ($recipes as $recipe) {
-            $recipe->tags()->attach($tags);
-        }
-
-        $generateDietPlan = new GenerateDietPlan('2022-09-30');
-        $generateDietPlan->handle($user);
-
-        $this->assertDatabaseCount('diet_plans', 1);
-        $this->assertDatabaseCount('meals', 4);
-    }
-
-    public function test_handle_with_a_date_but_without_a_user_given()
-    {
-        $users = User::factory()->count(2)->create();
-        $tags = [];
-        foreach ($users as $user) {
-            $tags = array_merge($tags, $user->userDiet->getMealsTags());
-        }
-        $recipes = Recipe::factory()->has(Tag::factory())->count(4)->create();
-        foreach ($recipes as $recipe) {
-            $recipe->tags()->attach($tags);
-        }
-        $generateDietPlan = new GenerateDietPlan('2022-09-30');
-
-        $generateDietPlan->handle();
-
-        $this->assertDatabaseCount('diet_plans', 2);
-        $this->assertDatabaseCount('meals', 8);
-    }
-
-    public function test_handle_without_a_date_but_with_a_user_given()
-    {
-        $user = User::factory()->create();
-        $tags = $user->userDiet->getMealsTags();
-        $recipes = Recipe::factory()->has(Tag::factory())->count(4)->create();
-        foreach ($recipes as $recipe) {
-            $recipe->tags()->attach($tags);
-        }
-        $generateDietPlan = new GenerateDietPlan();
-
-        $generateDietPlan->handle($user);
-
-        $this->assertDatabaseCount('diet_plans', 1);
-        $this->assertDatabaseCount('meals', 4);
-    }
-
-    public function test_handle_without_a_date_and_without_a_user_given()
-    {
-        $users = User::factory()->count(2)->create();
-        $tags = [];
-        foreach ($users as $user) {
-            $tags = array_merge($tags, $user->userDiet->getMealsTags());
-        }
-        $recipes = Recipe::factory()->has(Tag::factory())->count(4)->create();
-        foreach ($recipes as $recipe) {
-            $recipe->tags()->attach($tags);
-        }
-        $generateDietPlan = new GenerateDietPlan();
-
-        $generateDietPlan->handle();
-
-        $this->assertDatabaseCount('diet_plans', 2);
-        $this->assertDatabaseCount('meals', 8);
-    }
+//    public function test_handle_with_a_date_and_a_user_given()
+//    {
+//        $user = User::factory()->create();
+//        $tags = $user->userDiet->getMealsTags();
+//        $recipes = Recipe::factory()->has(Tag::factory())->count(4)->create();
+//        foreach ($recipes as $recipe) {
+//            $recipe->tags()->attach($tags);
+//        }
+//
+//        $generateDietPlan = new GenerateDietPlan('2022-09-30');
+//        $generateDietPlan->handle($user);
+//
+//        $this->assertDatabaseCount('diet_plans', 1);
+//        $this->assertDatabaseCount('meals', 4);
+//    }
+//
+//    public function test_handle_with_a_date_but_without_a_user_given()
+//    {
+//        $users = User::factory()->count(2)->create();
+//        $tags = [];
+//        foreach ($users as $user) {
+//            $tags = array_merge($tags, $user->userDiet->getMealsTags());
+//        }
+//        $recipes = Recipe::factory()->has(Tag::factory())->count(4)->create();
+//        foreach ($recipes as $recipe) {
+//            $recipe->tags()->attach($tags);
+//        }
+//        $generateDietPlan = new GenerateDietPlan('2022-09-30');
+//
+//        $generateDietPlan->handle();
+//
+//        $this->assertDatabaseCount('diet_plans', 2);
+//        $this->assertDatabaseCount('meals', 8);
+//    }
+//
+//    public function test_handle_without_a_date_but_with_a_user_given()
+//    {
+//        $user = User::factory()->create();
+//        $tags = $user->userDiet->getMealsTags();
+//        $recipes = Recipe::factory()->has(Tag::factory())->count(4)->create();
+//        foreach ($recipes as $recipe) {
+//            $recipe->tags()->attach($tags);
+//        }
+//        $generateDietPlan = new GenerateDietPlan();
+//
+//        $generateDietPlan->handle($user);
+//
+//        $this->assertDatabaseCount('diet_plans', 1);
+//        $this->assertDatabaseCount('meals', 4);
+//    }
+//
+//    public function test_handle_without_a_date_and_without_a_user_given()
+//    {
+//        $users = User::factory()->count(2)->create();
+//        $tags = [];
+//        foreach ($users as $user) {
+//            $tags = array_merge($tags, $user->userDiet->getMealsTags());
+//        }
+//        $recipes = Recipe::factory()->has(Tag::factory())->count(4)->create();
+//        foreach ($recipes as $recipe) {
+//            $recipe->tags()->attach($tags);
+//        }
+//
+//        GenerateDietPlan::dispatch();
+//
+//        $this->assertDatabaseCount('diet_plans', 2);
+//        $this->assertDatabaseCount('meals', 8);
+//    }
 }
