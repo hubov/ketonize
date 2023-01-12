cc3902a6880e214a235ddcd640ef29193a2122f6
Hubert Owerczuk <hub.ower@gmail.com>
2023-01-12 15:11:43 +0100

    Add method to get all active users

--- app/Repositories/Interfaces/UserRepositoryInterface.php
+++ app/Repositories/Interfaces/UserRepositoryInterface.php
@@ -3,8 +3,10 @@
 namespace App\Repositories\Interfaces;
 
 use App\Models\User;
+use Illuminate\Support\Collection;
 
 interface UserRepositoryInterface
 {
     public function get(int $id) : User;
+    public function getAllActive() : Collection;
 }
--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -4,11 +4,17 @@ namespace App\Repositories;
 
 use App\Models\User;
 use App\Repositories\Interfaces\UserRepositoryInterface;
+use Illuminate\Support\Collection;
 
 class UserRepository implements UserRepositoryInterface
 {
     public function get(int $id) : User
     {
         return User::find($id);
+    }
+
+    public function getAllActive(): Collection
+    {
+        return User::has('userDiet')->get();
     }
 }
