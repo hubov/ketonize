9df6781eba7a65fb94f6dd4e14adfef5fa768500
Hubert Owerczuk <hub.ower@gmail.com>
2023-01-13 15:25:34 +0100

    Remove redundant method in DietPlan controller

--- app/Http/Controllers/DietPlanController.php
+++ app/Http/Controllers/DietPlanController.php
@@ -57,16 +57,6 @@ class DietPlanController extends Controller
         ]);
     }
 
-    public function generate(Request $request)
-    {
-        $this->dietPlanService->setUser(Auth::user());
-        $this->dietPlanService->updateOnDate($request->date);
-
-        $url = ($request->date == date('Y-m-d')) ? '/dashboard' : '/dashboard/' . $request->date;
-
-        return redirect($url);
-    }
-
     public function update(Request $request)
     {
         $this->dietPlanService->setUser(Auth::user());
