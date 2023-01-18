1b561295e77d03fad6899e1295323caf89e9249b
Hubert Owerczuk <hub.ower@gmail.com>
2023-01-18 10:58:45 +0100

    Fix missing method in IngredientSearchService interface

--- app/Repositories/Interfaces/IngredientRepositoryInterface.php
+++ app/Repositories/Interfaces/IngredientRepositoryInterface.php
@@ -9,6 +9,7 @@ interface IngredientRepositoryInterface
 {
     public function get(int $id) : Ingredient;
     public function getAll() : Collection;
+    public function getByNameLimited(string $name, int $limit) : Collection;
     public function create(array $attributes) : Ingredient;
     public function update(int $id, array $attributes) : Ingredient;
     public function delete(int $id) : bool;
