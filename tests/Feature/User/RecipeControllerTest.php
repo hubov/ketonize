a7a45f0cb66615ad99418652413aea276ad4b017
Hubert Owerczuk <hub.ower@gmail.com>
2023-01-27 13:16:51 +0100

    Transfer admin feature tests related to Recipe controller to separate test class

--- tests/Feature/User/RecipeControllerTest.php
+++ tests/Feature/User/RecipeControllerTest.php
@@ -5,7 +5,6 @@ namespace Tests\Feature\User;
 use App\Models\Ingredient;
 use App\Models\Profile;
 use App\Models\Recipe;
-use App\Models\Role;
 use App\Models\Tag;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -65,41 +64,6 @@ class RecipeControllerTest extends TestCase
         $response->assertRedirect('/login');
     }
 
-    public function test_single_recipe_screen_for_admin_can_be_rendered()
-    {
-        $user = User::factory()->has(Profile::factory())->has(Role::factory()->state(['name' => 'admin']))->create();
-        $recipe = Recipe::factory()->create();
-        $response = $this->actingAs($user)->get('/recipe/'.$recipe->slug);
-
-        $response->assertViewHas('admin', TRUE);
-        $response->assertStatus(200);
-    }
-
-    public function test_adding_new_recipe_by_admin()
-    {
-        $user = User::factory()->has(Profile::factory())->has(Role::factory()->state(['name' => 'admin']))->create();
-        $ingredient = Ingredient::factory()->create();
-        $tag = Tag::factory()->create();
-        $amount = 100;
-
-        $requestData = [
-            'name' => 'Recipe #1',
-            'slug' => 'recipe-1',
-            'image' => '',
-            'description' => 'Some recipe description.',
-            'preparation_time' => 10,
-            'cooking_time' => 5,
-            'ids' => [$ingredient->id],
-            'quantity' => [$amount],
-            'tags' => [$tag->id]
-        ];
-
-        $request = $this->actingAs($user)->post('/recipes', $requestData);
-
-        $request->assertRedirect('/recipe/recipe-1');
-        $request->assertLocation('/recipe/recipe-1');
-    }
-
     public function test_prevent_adding_new_recipe_by_user()
     {
         $request = $this->actingAs($this->user)->post('/recipes', []);
@@ -112,45 +76,6 @@ class RecipeControllerTest extends TestCase
         $request = $this->post('/recipes', []);
 
         $request->assertStatus(403);
-    }
-
-    public function test_updating_recipe_by_admin()
-    {
-        $user = User::factory()->has(Profile::factory())->has(Role::factory()->state(['name' => 'admin']))->create();
-        $recipe = Recipe::factory()->hasAttached(Ingredient::factory(), ['amount' => '100'])->has(Tag::factory())->create();
-
-        $ingredientNew = Ingredient::factory()->create();
-        $tagNew = Tag::factory()->create();
-        $amount = 100;
-
-        foreach ($recipe->ingredients as $ingredient) {
-            $ingredients[] = $ingredient->id;
-            $amounts[] = $ingredient->pivot->amount;
-        }
-        $ingredients[] = $ingredientNew->id;
-        $amounts[] = $amount;
-
-        foreach ($recipe->tags as $tag) {
-            $tags[] = $tag->id;
-        }
-        $tags[] = $tagNew->id;
-
-        $requestData = [
-            'name' => 'Recipe #1',
-            'slug' => 'recipe-1',
-            'image' => '',
-            'description' => 'Some recipe description.',
-            'preparation_time' => 10,
-            'cooking_time' => 5,
-            'ids' => $ingredients,
-            'quantity' => $amounts,
-            'tags' => $tags
-        ];
-
-        $request = $this->actingAs($user)->put('/recipe/'.$recipe->slug.'/edit', $requestData);
-
-        $request->assertRedirect('/recipe/recipe-1');
-        $request->assertLocation('/recipe/recipe-1');
     }
 
     public function test_search_as_guest()
