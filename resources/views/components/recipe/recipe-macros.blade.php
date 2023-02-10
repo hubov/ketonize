ea1e2bfe75d6c354ad22a1aa136a332eed9ec2cc
Hubert Owerczuk <hub.ower@gmail.com>
2023-02-10 13:55:52 +0100

    Add new layout for single Recipe

--- resources/views/components/recipe/recipe-macros.blade.php
+++ resources/views/components/recipe/recipe-macros.blade.php
@@ -1,41 +1,213 @@
-<div class="card-text">
-    <div class="row macros">
-        <div class="row my-3">
-            <div class="col">
-                <span class="scalable bold" id="scalable1">{{ $protein }}</span> g<br />
+<div class="card-text macros" style="height: 100%">
+    <div class="row" style="height: 100%;  display: flex; justify-content: space-between">
+        <div class="row mb-3">
+            <div class="col-4">
+                <canvas id="proteinChart"></canvas>
+                <div class="visually-hidden">
+                    <span class="scalable bold" id="scalable1">{{ $protein }}</span> g<br />
+                </div>
                 <span class="label">proteins</span>
             </div>
-            <div class="vr"></div>
-            <div class="col">
-                <span class="scalable bold" id="scalable2">{{ $fat }}</span> g<br />
+            <div class="col-4">
+                <canvas id="fatChart"></canvas>
+                <div class="visually-hidden">
+                    <span class="scalable bold" id="scalable2">{{ $fat }}</span> g<br />
+                </div>
                 <span class="label">fats</span>
             </div>
-            <div class="vr"></div>
-            <div class="col">
-                <span class="scalable bold" id="scalable3">{{ $carbohydrate }}</span> g<br />
+            <div class="col-4">
+                <canvas id="carbsChart"></canvas>
+                <div class="visually-hidden">
+                    <span class="scalable bold" id="scalable3">{{ $carbohydrate }}</span> g<br />
+                </div>
                 <span class="label">carbohydrates</span>
             </div>
-            <div class="vr"></div>
+        </div>
+        <hr>
+        <div class="row mb-3">
             <div class="col">
-                <span class="bold">{{ $preparationTime }}</span> min<br />
-                <span class="label">preparation</span>
+                <div class="row">
+                    <span class="material-symbols-outlined">surgical</span>
+                </div>
+                <div class="row">
+                    <span class="py-2"><strong>{{ $preparationTime }}</strong> min</span><br />
+                    <span class="label">preparation</span>
+                </div>
             </div>
-            <div class="vr"></div>
             <div class="col">
-                <span class="bold">{{ $cookingTime }}</span> min<br />
-                <span class="label">cooking</span>
+                <div class="row">
+                    <span class="material-symbols-outlined">oven_gen</span>
+                </div>
+                <div class="row">
+                        <span class="py-2"><strong>{{ $cookingTime }}</strong> min</span><br />
+                    <span class="label">cooking</span>
+                </div>
+            </div>
+            <div class="col">
+                <div class="row">
+                    <span class="material-icons material-icons-outlined">schedule</span>
+                </div>
+                <div class="row">
+                    <span class="py-2"><strong>{{ $totalTime }}</strong> min</span><br />
+                    <span class="label">total</span>
+                </div>
             </div>
         </div>
-        <div class="row mt-3">
-            <div class="col">
-                <span class="material-icons material-icons-outlined inline-icon scale teal" direction="down">remove</span> <span class="scalable bold" id="scalable0">{{ $kcal }}</span> kcal <span class="material-icons material-icons-outlined inline-icon scale teal" direction="up">add</span><br />
-                <span class="label">calories</span>
+        <hr>
+        <div class="row mb-3 px-0">
+            <div class="col px-1">
+                <div class="card" style="background-color: #155263; color: #fff; padding: 10px">
+                        <div class="row">
+                            <span class="material-icons material-icons-outlined yellow">local_fire_department</span>
+                        </div>
+                        <div class="row">
+                            <span class="py-2">
+                                <i class="material-icons material-icons-outlined inline-icon scale" direction="down">remove</i> <strong class="scalable" id="scalable0">{{ $kcal }}</strong> kcal <i class="material-icons material-icons-outlined inline-icon scale" direction="up">add</i>
+                            </span>
+                            <br />
+                            <span class="label">calories</span>
+                        </div>
+                </div>
             </div>
             <div class="col">
-                <span class="material-icons material-icons-outlined inline-icon scale teal" direction="down">remove</span> <span class="scalable bold" id="scalable4">{{ $weightTotal }}</span> g <span class="material-icons material-icons-outlined inline-icon scale teal" direction="up">add</span><br />
-                <span class="label">amount</span>
+                <div class="card" style="background-color: #155263; color: #fff; padding: 10px">
+                    <div class="row">
+                        <span class="material-symbols-outlined yellow">scale</span>
+                    </div>
+                    <div class="row">
+                        <span class="py-2">
+                            <i class="material-icons material-icons-outlined inline-icon scale" direction="down">remove</i> <strong class="scalable" id="scalable4">{{ $weightTotal }}</strong> g <i class="material-icons material-icons-outlined inline-icon scale" direction="up">add</i>
+                        </span>
+                        <br />
+                        <span class="label">amount</span>
+                    </div>
+                </div>
             </div>
         </div>
+        @if (count($tags))
+            <hr>
+            <div class="row">
+                <span>
+                    @foreach($tags as $tag)
+                        <span class="badge">{{ $tag->name }}</span>
+                    @endforeach
+                </span>
+            </div>
+        @endif
     </div>
 </div>
-<hr>
+
+<script src="
+https://cdn.jsdelivr.net/npm/chart.js@4.2.0/dist/chart.umd.min.js
+"></script>
+<script>
+    var macroCenter = {
+        'id': 'center_macro_value',
+        beforeDraw: function(chart) {
+            var width = chart.width,
+                height = chart.height,
+                ctx = chart.ctx,
+                value = chart.data.datasets[0].data[0];
+
+            ctx.restore();
+            var fontSize = (height / 70).toFixed(2);
+            ctx.font = fontSize + "em sans-serif";
+            ctx.textBaseline = "middle";
+
+            var text = value + "g",
+                textX = Math.round((width - ctx.measureText(text).width) / 2),
+                textY = height / 1.85;
+
+            ctx.fillStyle = '#fff';
+            ctx.fillText(text, textX, textY);
+            ctx.save();
+        }
+    };
+
+    Chart.register(macroCenter);
+
+    var options = {
+        responsive: true,
+        legend: {
+            display: false
+        },
+        elements: {
+            arc: {
+                borderWidth: 0
+            }
+        },
+        cutout: '75%',
+        events: [],
+        plugins: [macroCenter]
+    };
+
+    var protein = {{ $protein }};
+    var fat = {{ $fat }};
+    var carbs = {{ $carbohydrate }};
+    var macroSum = protein + fat + carbs;
+
+    var dataProtein = {
+        datasets: [
+            {
+                data: [protein, macroSum-protein],
+                backgroundColor: ['#ffc93c', '#155263']
+            }
+        ]
+    };
+
+    var proteinChart = new Chart(document.getElementById('proteinChart'), {
+        type: 'doughnut',
+        data: dataProtein,
+        options: options
+    });
+
+    var dataFat = {
+        datasets: [
+            {
+                data: [fat, macroSum-fat],
+                backgroundColor: ['#ffc93c', '#155263']
+            }
+        ]
+    };
+
+    var fatChart = new Chart(document.getElementById('fatChart'), {
+        type: 'doughnut',
+        data: dataFat,
+        options: options
+    });
+
+    var dataCarbs = {
+        datasets: [
+            {
+                data: [carbs, macroSum-carbs],
+                backgroundColor: ['#ffc93c', '#155263']
+            }
+        ]
+    };
+
+    var carbsChart = new Chart(document.getElementById('carbsChart'), {
+        type: 'doughnut',
+        data: dataCarbs,
+        options: options
+    });
+
+    setInterval(updateCharts, 100);
+
+    function updateCharts()
+    {
+        updateMacroChart(proteinChart, $('#scalable1').text(), macrosSum());
+        updateMacroChart(fatChart, $('#scalable2').text(), macrosSum());
+        updateMacroChart(carbsChart, $('#scalable3').text(), macrosSum());
+    }
+
+    function macrosSum() {
+        return parseInt($('#scalable1').text()) + parseInt($('#scalable2').text()) + parseInt($('#scalable3').text());
+    }
+
+    function updateMacroChart(chart, value, sum)
+    {
+        chart.data.datasets[0].data[0] = value;
+        chart.data.datasets[0].data[1] = sum;
+        chart.update();
+    }
+</script>
--- resources/views/components/recipe/recipe-preview.blade.php
+++ resources/views/components/recipe/recipe-preview.blade.php
@@ -1,46 +1,54 @@
-<img src="{{ asset($image) }}" id="recipeImage" class="card-img-top" alt="...">
-<div class="card-body">
-    <h1 class="card-title" id="recipeName">{{ $name }}
-        @if ($admin)
-            <a href="{!! url()->current() !!}/edit"><span class="material-icons material-icons-outlined">edit</span></a>
-        @endif
-    </h1>
-    @if ($displayMacros == true)
-        <x-recipe.recipe-macros :protein="$protein" :fat="$fat" :carbohydrate="$carbohydrate" :kcal="$kcal" :weightTotal="$weightTotal" :preparationTime="$preparationTime" :cookingTime="$cookingTime" />
-    @endif
-    <div class="card-text mt-4">
-        <div class="row ingredients">
-            <div class="col">
-                <div class="row">
-                    <h5>Ingredients</h5>
-                </div>
-                <div class="row mx-sm-4" id="recipeIngredients">
-                    @php
-                        $scalableCount = 5;
-                    @endphp
-                    @if ($ingredients)
-                        @foreach ($ingredients as $ingredient)
-                            <x-ingredients-list-element :name="$ingredient->name" :scalableCount="$scalableCount" :amount="$ingredient->pivot->amount" :symbol="$ingredient->unit->symbol" />
-                            @php
-                                $scalableCount++;
-                            @endphp
-                        @endforeach
-                    @endif
-                </div>
+<div class="row g-0" style="background-color: #155263; color: #fff">
+    <div class="col-md-8 img-container">
+        <div class="img-overlay"></div>
+        <img src="{{ asset($image) }}" class="img-fluid rounded-start" id="recipeImage">
+        <div class="overlay-content-bl"><h1 class="card-title white" id="recipeName">{{ $name }}
+                @if ($admin)
+                    <a href="{!! url()->current() !!}/edit" class="white"><span class="material-icons material-icons-outlined">edit</span></a>
+                @endif
+            </h1></div>
+    </div>
+    <div class="col-md-4">
+        <div class="card-body" style="height: 100%">
+            @if ($displayMacros == true)
+                <x-recipe.recipe-macros :protein="$protein" :fat="$fat" :carbohydrate="$carbohydrate" :kcal="$kcal" :weightTotal="$weightTotal" :preparationTime="$preparationTime" :cookingTime="$cookingTime" :totalTime="$totalTime" :tags="$tags" />
+            @endif
+        </div>
+    </div>
+</div>
+</div>
+<div class="card-text mt-4">
+    <div class="row ingredients">
+        <div class="col">
+            <div class="row">
+                <h5>Ingredients</h5>
+            </div>
+            <div class="row mx-sm-4" id="recipeIngredients">
+                @php
+                    $scalableCount = 5;
+                @endphp
+                @if ($ingredients)
+                    @foreach ($ingredients as $ingredient)
+                        <x-ingredients-list-element :name="$ingredient->name" :scalableCount="$scalableCount" :amount="$ingredient->pivot->amount" :symbol="$ingredient->unit->symbol" />
+                        @php
+                            $scalableCount++;
+                        @endphp
+                    @endforeach
+                @endif
             </div>
         </div>
     </div>
-    <hr>
-    <div class="card-text mt-4">
-        <div class="row">
-            <h5>Preparation</h5>
-        </div>
-        <div class="row mx-sm-4">
-            <div class="col" id="recipeDescription">
-                @if ($description)
-                    {!! nl2br($description) !!}
-                @endif
-            </div>
+</div>
+<hr>
+<div class="card-text mt-4">
+    <div class="row">
+        <h5>Preparation</h5>
+    </div>
+    <div class="row mx-sm-4">
+        <div class="col" id="recipeDescription">
+            @if ($description)
+                {!! nl2br($description) !!}
+            @endif
         </div>
     </div>
 </div>
