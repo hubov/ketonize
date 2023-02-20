6a8da519dd37c4f9b21a18118e9fe7de68bb0195
Hubert Owerczuk <hub.ower@gmail.com>
2023-02-20 11:14:11 +0100

    Add new layout to ShoppingList view

--- resources/views/shopping-list.blade.php
+++ resources/views/shopping-list.blade.php
@@ -1,83 +1,119 @@
 <x-app-layout>
-	<div class="container">
-		<form method="POST" class="mt-3">
-		@csrf
-		<div class="row">
-			<div class="col">
-				<label for="date_from" class="form-label">Start date</label>
-				<input type="date" class="form-control" id="date_from" name="date_from" value="{{ $date_from }}">
-			</div>
-			<div class="col">
-				<label for="date_to" class="form-label">End date</label>
-				<input type="date" class="form-control" id="date_to" name="date_to" value="{{ $date_to }}">
-			</div>
-			<div class="col">
-				<input type="submit" class="btn btn-primary" id="generate" value="Generate New List">
-			</div>
-		</div>
-		</form>
-		<div class="row justify-content-md-center">
-			<div class="col-xxl-6 col-xl-8 col-lg-10">
-				@if (count($list) > 0)
-					<table class="table table-striped mt-4">
-						<thead>
-							<tr>
-								<th scope="col">Ingredient</th>
-								<th scope="col">Amount</th>
-								<th></th>
-							</tr>
-						</thead>
-						<tbody>
-					@php
-						$categoriesCount = 0;
-						$scalablesCount = 0;
-					@endphp
-			        @foreach ($list as $group => $elements)
-			        	@php
-			        		$categoriesCount++;
-			        	@endphp
-			        	<tr class="table-primary" cat-id="{{ $categoriesCount }}">
-			        		<td colspan="3">
-			        			<b>{{ $group }}</b>
-			        		</td>
-			        	</tr>
-			        	@foreach ($elements as $element)
-			        		<tr>
-			        			<td scope="row">{{ $element->ingredient->name }}</td>
-			        			<td>
-			        				<span class="material-icons material-icons-outlined inline-icon scale" direction="down" scale="{{ $scalablesCount }}">
-			        					remove
-			        				</span>
-			        				<span id="scalable{{ $scalablesCount }}" class="scalable" ingredient-id="{{ $element->id }}">
-			        					{{ $element->amount }}
-			        				</span>
-			        				{{ $element->ingredient->unit->symbol }}
-			        				<span class="material-icons material-icons-outlined inline-icon scale" direction="up" scale="{{ $scalablesCount }}">
-			        					add
-			        				</span>
-			        			</td>
-			        			<td><span class="material-icons material-icons-outlined text-danger inline-icon remover" cat-id="{{ $categoriesCount }}">clear</span></td>
-			        		</tr>
-			        		@php
-								$scalablesCount++;
-							@endphp
-						@endforeach
-					@endforeach
-						</tbody>
-					</table>
-			    @else
-                    <div class="row mt-4">
-                        <div class="alert alert-success d-flex align-items-center" role="alert">
-                            <span class="material-icons material-icons-outlined bi flex-shrink-0 me-2">task_alt</span>
-                            <div>
-                                Your shopping list is empty! Well done!
+    <div class="row justify-content-md-center mt-3">
+        <div class="col-xxl-6 col-xl-8 col-lg-10 shopping-list-menu">
+            <div class="row align-middle mb-3">
+                <div class="col">
+                    <button class="btn btn-primary me-2" type="button" data-bs-toggle="collapse" data-bs-target="#generateList" aria-expanded="false" aria-controls="generateList">
+                        <span class="material-symbols-outlined inline-icon">list</span><span class="d-none d-md-inline"> New list</span>
+                    </button>
+                    <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                        <span class="material-symbols-outlined inline-icon">playlist_add</span><span class="d-none d-md-inline"> Add item</span>
+                    </button>
+                </div>
+                <div class="col position-relative">
+                    <span class="position-absolute end-0 top-50 translate-middle-y">
+                        <div class="form-check form-switch">
+                            <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+                            <label class="form-check-label" for="flexSwitchCheckDefault"><span class="material-symbols-outlined inline-icon teal">sync</span></label>
+                        </div>
+                    </span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="collapse p-0" id="generateList">
+                    <div class="card card-body mb-4">
+                        <form method="POST">
+                            <div class="row">
+                                @csrf
+                                <div class="col my-2">
+                                    <label for="date_from" class="form-label">Start date</label>
+                                    <input type="date" class="form-control w-100" id="date_from" name="date_from" value="{{ $date_from }}">
+                                </div>
+                                <div class="col my-2">
+                                    <label for="date_to" class="form-label">End date</label>
+                                    <input type="date" class="form-control w-100" id="date_to" name="date_to" value="{{ $date_to }}">
+                                </div>
+                                <div class="col my-2 d-flex align-items-end">
+                                    <input type="submit" class="btn btn-primary w-100" id="generate" value="Generate New List">
+                                </div>
                             </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-1 mb-2">
+                <h1>Shopping list</h1>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-md-center">
+        <div class="col-xxl-6 col-xl-8 col-lg-10 p-4 border bg-white">
+            @if (count($list) > 0)
+                <table class="table table-borderless">
+                    <thead>
+                        <tr>
+                            <th scope="col" class="border-0">Ingredient</th>
+                            <th scope="col"class="border-0">Amount</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                @php
+                    $categoriesCount = 0;
+                    $scalablesCount = 0;
+                @endphp
+                @foreach ($list as $group => $elements)
+                    @php
+                        $categoriesCount++;
+                    @endphp
+                    <tr class="table-category" cat-id="{{ $categoriesCount }}">
+                        <td colspan="3">
+                            <div>
+                                <b>{{ $group }}</b>
+                            </div>
+                        </td>
+                    </tr>
+                    @foreach ($elements as $element)
+                        <tr>
+                            <td scope="row">{{ $element->ingredient->name }}</td>
+                            <td class="bold text-center">
+                                <p class="material-symbols-outlined inline-icon scale w-100 m-3 teal" direction="up" scale="{{ $scalablesCount }}" style="display: none">
+                                    add_box
+                                </p>
+                                <div class="scalable_steering">
+                                    <span id="scalable{{ $scalablesCount }}" class="scalable" ingredient-id="{{ $element->id }}">
+                                        {{ $element->amount }}
+                                    </span>
+                                    {{ $element->ingredient->unit->symbol }}
+                                    <span class="material-symbols-outlined inline-icon">
+                                    edit
+                                    </span>
+                                </div>
+                                <p class="material-symbols-outlined inline-icon scale text-center w-100 m-3 teal" direction="down" scale="{{ $scalablesCount }}" style="display: none">
+                                    indeterminate_check_box
+                                </p>
+                            </td>
+                            <td><span class="material-icons material-icons-outlined text-danger inline-icon remover" cat-id="{{ $categoriesCount }}">clear</span></td>
+                        </tr>
+                        @php
+                            $scalablesCount++;
+                        @endphp
+                    @endforeach
+                @endforeach
+                    </tbody>
+                </table>
+            @else
+                <div class="row mt-4">
+                    <div class="alert alert-success d-flex align-items-center" role="alert">
+                        <span class="material-icons material-icons-outlined bi flex-shrink-0 me-2">task_alt</span>
+                        <div>
+                            Your shopping list is empty! Well done!
                         </div>
                     </div>
-			    @endif
-			</div>
-		</div>
-	</div>
+                </div>
+            @endif
+        </div>
+    </div>
 </x-app-layout>
 
 <x-scalables-script />
@@ -110,5 +146,10 @@
                 console.log('fail');
             });
     	});
+    });
+
+    $('.scalable_steering').on('click', function() {
+        console.log('click');
+        $(this).parent().find('.scale').toggle(300);
     });
 </script>
