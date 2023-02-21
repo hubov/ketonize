7184687eaddb2d68c8285f7a750c43ef5a7465d4
Hubert Owerczuk <hub.ower@gmail.com>
2023-02-21 12:01:57 +0100

    Extract ShoppingList item to blade component

---
+++ resources/views/components/shopping-list-row.blade.php
@@ -0,0 +1,25 @@
+<tr>
+    <td scope="row">{{ $element->ingredient->name }}</td>
+    <td class="bold text-center">
+        <p class="material-symbols-outlined inline-icon scale w-100 m-3 teal" direction="up" scale="{{ $scalablesCount }}" style="display: none">
+            add_box
+        </p>
+        <div class="scalable_steering">
+                                        <span id="scalable{{ $scalablesCount }}" class="scalable" ingredient-id="{{ $element->id }}">
+                                            {{ $element->amount }}
+                                        </span>
+            {{ $element->ingredient->unit->symbol }}
+            <span class="material-symbols-outlined inline-icon">
+                                        edit
+                                        </span>
+        </div>
+        <p class="material-symbols-outlined inline-icon scale text-center w-100 m-3 teal" direction="down" scale="{{ $scalablesCount }}" style="display: none">
+            indeterminate_check_box
+        </p>
+    </td>
+    <td class="text-center">
+        <span class="material-icons material-icons-outlined text-danger inline-icon remover" cat-id="{{ $categoryId }}">clear</span>
+        <span class="material-symbols-outlined inline-icon teal redo me-4" cat-id="{{ $categoryId }}">redo</span>
+        <span class="material-symbols-outlined inline-icon text-danger destroy" cat-id="{{ $categoryId }}">delete_forever</span>
+    </td>
+</tr>
--- resources/views/shopping-list.blade.php
+++ resources/views/shopping-list.blade.php
@@ -66,31 +66,7 @@
                     </tr>
                     @if (isset($list[$category->id]))
                         @foreach ($list[$category->id] as $element)
-                            <tr>
-                                <td scope="row">{{ $element->ingredient->name }}</td>
-                                <td class="bold text-center">
-                                    <p class="material-symbols-outlined inline-icon scale w-100 m-3 teal" direction="up" scale="{{ $scalablesCount }}" style="display: none">
-                                        add_box
-                                    </p>
-                                    <div class="scalable_steering">
-                                        <span id="scalable{{ $scalablesCount }}" class="scalable" ingredient-id="{{ $element->id }}">
-                                            {{ $element->amount }}
-                                        </span>
-                                        {{ $element->ingredient->unit->symbol }}
-                                        <span class="material-symbols-outlined inline-icon">
-                                        edit
-                                        </span>
-                                    </div>
-                                    <p class="material-symbols-outlined inline-icon scale text-center w-100 m-3 teal" direction="down" scale="{{ $scalablesCount }}" style="display: none">
-                                        indeterminate_check_box
-                                    </p>
-                                </td>
-                                <td class="text-center">
-                                    <span class="material-icons material-icons-outlined text-danger inline-icon remover" cat-id="{{ $category->id }}">clear</span>
-                                    <span class="material-symbols-outlined inline-icon teal redo me-4" cat-id="{{ $category->id }}">redo</span>
-                                    <span class="material-symbols-outlined inline-icon text-danger destroy" cat-id="{{ $category->id }}">delete_forever</span>
-                                </td>
-                            </tr>
+                            <x-shopping-list-row :$element :$scalablesCount :categoryId="$category->id" />
                             @php
                                 $scalablesCount++;
                             @endphp
@@ -102,31 +78,10 @@
                     <tbody id="trashedShoppingList">
                         @foreach ($trashed as $trashedList)
                             @foreach ($trashedList as $categoryId => $element)
-                                <tr>
-                                    <td scope="row">{{ $element->ingredient->name }}</td>
-                                    <td class="bold text-center">
-                                        <p class="material-symbols-outlined inline-icon scale w-100 m-3 teal" direction="up" scale="{{ $scalablesCount }}" style="display: none">
-                                            add_box
-                                        </p>
-                                        <div class="scalable_steering">
-                                        <span id="scalable{{ $scalablesCount }}" class="scalable" ingredient-id="{{ $element->id }}">
-                                            {{ $element->amount }}
-                                        </span>
-                                            {{ $element->ingredient->unit->symbol }}
-                                            <span class="material-symbols-outlined inline-icon">
-                                        edit
-                                        </span>
-                                        </div>
-                                        <p class="material-symbols-outlined inline-icon scale text-center w-100 m-3 teal" direction="down" scale="{{ $scalablesCount }}" style="display: none">
-                                            indeterminate_check_box
-                                        </p>
-                                    </td>
-                                    <td class="text-center">
-                                        <span class="material-icons material-icons-outlined text-danger inline-icon remover" cat-id="{{ $categoryId }}">clear</span>
-                                        <span class="material-symbols-outlined inline-icon teal redo me-4" cat-id="{{ $categoryId }}">redo</span>
-                                        <span class="material-symbols-outlined inline-icon text-danger destroy" cat-id="{{ $categoryId }}">delete_forever</span>
-                                    </td>
-                                </tr>
+                                <x-shopping-list-row :$element :$scalablesCount :categoryId="$element->ingredient->ingredient_category_id" />
+                                @php
+                                    $scalablesCount++;
+                                @endphp
                             @endforeach
                         @endforeach
                     </tbody>
