dab65caedd1fecdd37fe81169c7113c7aa425c63
Hubert Owerczuk <hub.ower@gmail.com>
2023-02-21 08:22:14 +0100

    Add IngredientCategory service

---
+++ app/Services/IngredientCategoryService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\Interfaces\IngredientCategoryRepositoryInterface;
+use App\Services\Interfaces\IngredientCategoryInterface;
+
+class IngredientCategoryService implements IngredientCategoryInterface
+{
+    protected $ingredientCategoryRepository;
+
+    public function __construct(IngredientCategoryRepositoryInterface $ingredientCategoryRepository)
+    {
+        $this->ingredientCategoryRepository = $ingredientCategoryRepository;
+    }
+
+    public function getAllByName() : array
+    {
+        $rawResults = $this->ingredientCategoryRepository
+            ->getAll()
+            ->sortBy('name');
+
+        foreach ($rawResults as $result) {
+            $results[$result->name] = $result;
+        }
+
+        return $results;
+    }
+}
