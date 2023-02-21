8a6b48b2f5b12b50cf18331ee21c35a4928ce145
Hubert Owerczuk <hub.ower@gmail.com>
2023-02-21 13:00:48 +0100

    Add mouse pointer style to clickable symbols in ShoppingList

--- resources/views/components/shopping-list-row.blade.php
+++ resources/views/components/shopping-list-row.blade.php
@@ -18,8 +18,8 @@
         </p>
     </td>
     <td class="text-center">
-        <span class="material-icons material-icons-outlined text-danger inline-icon remover" cat-id="{{ $categoryId }}">clear</span>
-        <span class="material-symbols-outlined inline-icon teal redo me-4" cat-id="{{ $categoryId }}">redo</span>
-        <span class="material-symbols-outlined inline-icon text-danger destroy" cat-id="{{ $categoryId }}">delete_forever</span>
+        <span class="material-icons material-icons-outlined text-danger inline-icon remover clickable" cat-id="{{ $categoryId }}">clear</span>
+        <span class="material-symbols-outlined inline-icon teal redo me-4 clickable" cat-id="{{ $categoryId }}">redo</span>
+        <span class="material-symbols-outlined inline-icon text-danger destroy clickable" cat-id="{{ $categoryId }}">delete_forever</span>
     </td>
 </tr>
