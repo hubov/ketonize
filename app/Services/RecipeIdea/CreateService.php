f02d390c397c8edbadcaa8fd7243a9af9b272a08
Hubert Owerczuk <hub.ower@gmail.com>
2023-03-29 11:44:17 +0200

    Add aiService to RecipeIdea creation service

--- app/Services/RecipeIdea/CreateService.php
+++ app/Services/RecipeIdea/CreateService.php
@@ -9,14 +9,87 @@ use App\Services\Interfaces\RecipeIdeaInterface;
 class CreateService implements RecipeIdeaInterface
 {
     protected $aiService;
+    protected $settings;
+    protected $context = [
+        'start' => 'napisz przepis na ',
+        'carbsType' => '',
+        'dietType' => '',
+        'finish' => ' alternatywę dania o nazwie podanej przez użytkownika. Ilość składników podaj w gramach lub mililitrach. Temperaturę w Celsjuszach. Podaj odpowiedź według wzoru poniżej:
+"""
+Ketogeniczne wegańskie ravioli
+~
+200g - mąka migdałowa
+50g - masło
+75g - szpinak
+3g - czosnek
+1g - sól morska
+1g - pieprz czarny
+~
+1. Połącz mąkę z masłem i wyrób ciasto.
+2. Czosnek podsmaż, dodaj szpinak i przyprawy.
+3. Rozwałkuj ciasto i zawiń w nim smażony szpinak.
+~
+90 kcal
+Białko: 6g
+Tłuszcz: 10g
+Węglowodany netto: 4g
+"""'
+    ];
+    protected $aiResult;
+    protected $parsedAiResult;
 
     public function __construct(AIGeneratorInterface $aiService)
     {
         $this->aiService = $aiService;
     }
 
+    public function setDiet(int $carbsId, int $dietTypeId): self
+    {
+        // get $carbs by id
+        $carbs = 'ketogeniczną';
+
+        // get $dietType by id
+        $dietType = 'wegańską';
+
+        $this->context['carbsType'] = ' ' . $carbs . ' ';
+        $this->context['dietType'] = ' ' . $dietType . ' ';
+
+        return $this;
+    }
+
+    public function execute(string $name): self
+    {
+        $settings = [
+            'messages' => $this->setMessages($name)
+        ];
+
+        $this->aiResult = $this->aiService
+            ->settings($settings)
+            ->execute()
+            ->return();
+
+        $this->parseAiResult();
+
+        return $this;
+    }
+
+    protected function setMessages(string $name): array
+    {
+       return  [
+            [
+                'role' => 'system',
+                'content' => implode('', $this->context)
+            ],
+            [
+                'role' => 'user',
+                'content' => trim($name)
+            ]
+        ];
+    }
     public function return(): RecipeIdea
     {
-        return new RecipeIdea();
+        $recipeIdea = new RecipeIdea();
+
+        return $recipeIdea;
     }
 }
--- tests/Unit/Services/RecipeIdea/CreateServiceTest.php
+++ tests/Unit/Services/RecipeIdea/CreateServiceTest.php
@@ -9,13 +9,64 @@ use PHPUnit\Framework\TestCase;
 
 class CreateServiceTest extends TestCase
 {
+    protected $aiResult;
+    protected $chatCompletionsService;
+
+    protected function setUp(): void
+    {
+        $this->aiResult = 'Ketogeniczne wegańskie gołąbki
+~
+150g - kasza jaglana
+150g - tofu
+150g - kapusta włoska
+50g - cebula
+3g - czosnek
+15ml - olej kokosowy
+1g - sól morska
+1g - pieprz czarny
+~
+1. Kaszę ugotuj według instrukcji na opakowaniu.
+2. W międzyczasie, cebulę i czosnek posiekaj, a tofu posiekaj lub rozetrzyj widelcem.
+3. Na patelni rozgrzej olej kokosowy i podsmaż cebulę i czosnek przez około 2-3 minuty.
+4. Dodaj do patelni tofu i smaż przez kolejne 5 minut.
+5. Kapustę włoską poszatkuj i blanszuj przez około 3 minuty.
+6. Wszystkie składniki połącz i dopraw solą i pieprzem.
+7. Z powstałej masy formuj gołąbki.
+8. Gotuj gołąbki w osolonej wodzie przez 20-25 minut lub dopóki nie będą miękkie.
+~
+90 kcal
+Białko: 6g
+Tłuszcz: 7g
+Węglowodany netto: 8g';
+
+        $this->chatCompletionsService = $this->createMock(AITextGeneratorInterface::class);
+        $this->chatCompletionsService
+            ->expects($this->once())
+            ->method('settings')
+            ->withAnyParameters()
+            ->willReturnSelf();
+        $this->chatCompletionsService
+            ->expects($this->once())
+            ->method('execute')
+            ->willReturnSelf();
+        $this->chatCompletionsService
+            ->expects($this->once())
+            ->method('return')
+            ->willReturn($this->aiResult);
+    }
+
     /** @test */
     public function returns_recipe_idea_object(): void
     {
-        $chatCompletionsService = $this->createMock(AITextGeneratorInterface::class);
+        $createService = new CreateService($this->chatCompletionsService);
+        $result = $createService
+            ->setDiet(1, 1)
+            ->execute('gołąbki')
+            ->return();
 
-        $createService = new CreateService($chatCompletionsService);
+        $this->assertIsObject($result);
+        $this->assertInstanceOf(RecipeIdea::class, $result);
 
-        $this->assertEquals(new RecipeIdea(), $createService->return());
+
     }
 }
